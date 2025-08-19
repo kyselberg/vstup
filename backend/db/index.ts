6edce3e5e64eda818c582fa336317ec92dea2e81
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-08-19 17:38:45 +0300

    Refactor environment variable handling in main application
    - Replaced manual dotenv configuration with a simpler import statement for loading environment variables.
    - Removed unnecessary console.log statement in db/index.ts to clean up the code.

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
-console.log(process.env);
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = createClient({
