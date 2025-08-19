dbab3dee033624a1a36043d70024fa4deb84cba1
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-08-19 18:14:35 +0300

    Enhance build script to include build step before database commands
    - Added a build command to the backend build script to ensure the application is built prior to executing database generation, migration, and push commands.

--- scripts/be.build.sh
+++ scripts/be.build.sh
@@ -4,10 +4,11 @@ cd backend
 
 pnpm install
 
+pnpm run build
+
 npx drizzle-kit generate --config ./drizzle.config.ts
 npx drizzle-kit migrate --config ./drizzle.config.ts
 npx drizzle-kit push --config ./drizzle.config.ts
 
-pnpm run build
 
 cd ..
