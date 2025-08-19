8a3f65c1ad0abbaeab45b077991fac4243116b81
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-08-19 16:35:07 +0300

    Add Drizzle ORM commands to build script for database management
    - Integrated commands to generate, migrate, and push database schema using Drizzle ORM.
    - Updated `be.build.sh` to streamline backend setup process.

--- scripts/be.build.sh
+++ scripts/be.build.sh
@@ -3,4 +3,9 @@
 cd backend
 pnpm install
 pnpm run build
+
+npx drizzle-kit generate --config ./drizzle.config.ts
+npx drizzle-kit migrate --config ./drizzle.config.ts
+npx drizzle-kit push --config ./drizzle.config.ts
+
 cd ..
