56993ee91ac55d2be5473cc5cd55e0e0fa204a44
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-08-20 10:48:48 +0300

    Enhance sorting and filtering logic in sort utility
    - Introduced a state priority mapping for improved sorting of program states in the sort utility.
    - Updated filter functions to utilize a predefined list of positive states, enhancing the filtering logic for program states.

--- frontend/src/utils/sort.ts
+++ frontend/src/utils/sort.ts
@@ -1,5 +1,7 @@
 import type { ProgramsType } from "../schemas/programs.js";
 
+const positiveStates = ['До наказу (контракт)', 'До наказу (бюджет)', 'Рекомендовано (бюджет)', 'Рекомендовано (контракт)', 'Зареєстровано', 'Допущено'];
+
 const sortByType = (a: { type: string }, b: { type: string }) => {
     if (a.type === 'Б' && b.type === 'К') return -1;
     if (a.type === 'К' && b.type === 'Б') return 1;
@@ -11,9 +13,19 @@ const sortByMarks = (a: { marks: string }, b: { marks: string }) => {
 }
 
 const sortByState = (a: { state: string }, b: { state: string }) => {
-    if (a.state === 'Прийнято' && b.state !== 'Прийнято') return -1;
-    if (b.state === 'Прийнято' && a.state !== 'Прийнято') return 1;
-    return 0;
+    const statePriority: Record<string, number> = {
+        'До наказу (бюджет)': 5,
+        'До наказу (контракт)': 4,
+        'Рекомендовано (бюджет)': 3,
+        'Рекомендовано (контракт)': 2,
+        'Зареєстровано': 1,
+        'Допущено': 0,
+    };
+
+    const priorityA = statePriority[a.state] ?? -1;
+    const priorityB = statePriority[b.state] ?? -1;
+
+    return priorityB - priorityA;
 }
 
 const filterByState = (row: { state: string }) => {
@@ -21,11 +33,11 @@ const filterByState = (row: { state: string }) => {
 }
 
 const filterAllowed = (row: { state: string }) => {
-    return row.state === "Допущено";
+    return positiveStates.includes(row.state);
 }
 
 const filterAllButAllowed = (row: { state: string }) => {
-    return row.state !== "Допущено";
+    return !positiveStates.includes(row.state);
 }
 
 export const processTable = (program: ProgramsType['programs'][number]) => {
